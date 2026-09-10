3dc4b531b7583824871b2f2281fe693d3a3277c4
reallegitdev <notaphish@reallegit.email>
2026-09-10 12:03:05 -0700

    Update Web data model search template

--- templates/web-template.js
+++ templates/web-template.js
@@ -1,4 +1,13 @@
-window.WEB_TEMPLATE = `| tstats \`summariesonly\` c as count min(_time) as first_seen, max(_time) as last_seen, values(Web.user), values(Web.uri_path), values(Web.http_method), values(Web.action), values(Web.status), values(Web.uri) values(Web.http_referrer) values(Web.http_user_agent) from datamodel=Web where nodename=Web {{TIME_RANGE}} Web.url IN ({{IOC_LIST}}) by Web.url Web.src Web.dest
-| rename values(Web.*) as *, Web.* as *
-| eval last_seen = strftime (last_seen, "%b %d, %H:%M %p")
-| eval first_seen = strftime (first_seen, "%b %d, %H:%M %p")`;
+window.WEB_TEMPLATE = `| tstats summariesonly=t count
+    min(_time) as first_seen
+    max(_time) as last_seen
+    from datamodel=Web.Web
+    where
+    (
+{{IOC_LIST}}
+    )
+    {{TIME_RANGE}}
+    by Web.url Web.src Web.dest
+| convert ctime(first_seen) ctime(last_seen)
+| rename Web.url as url Web.src as src Web.dest as dest
+| sort - count`;
