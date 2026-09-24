1500491a44c0d10ee948af13955cefdae5409125
reallegitdev <notaphish@reallegit.email>
2026-09-24 13:18:23 -0700

    Fix DNS tstats grouping for portability

--- templates/dns-template.js
+++ templates/dns-template.js
@@ -1,16 +1,18 @@
 window.DNS_TEMPLATE = `| tstats summariesonly=t count
     min(_time) as first_seen
     max(_time) as last_seen
+    values(DNS.dest) as dest
     values(DNS.answer) as answer
     values(DNS.reply_code) as reply_code
     values(DNS.record_type) as record_type
+    values(sourcetype) as sourcetype
     from datamodel=Network_Resolution.DNS
     where
     (
 {{IOC_LIST}}
     )
     {{TIME_RANGE}}
-    by DNS.query DNS.src DNS.dest
+    by DNS.query DNS.src
 | convert ctime(first_seen) ctime(last_seen)
-| rename DNS.query as query DNS.src as src DNS.dest as dest
+| rename DNS.query as query DNS.src as src
 | sort - count`;
